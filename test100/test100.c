66fa62ed08e0a907eba010663ad736b66994e4a6
Kim SeonHee <sh011226@naver.com>
2021-03-18 23:05:53 +0900

    third

--- test100/test100.c
+++ test100/test100.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 int main()
 {
-	int i, j;
-	for(i=5; i>=1; i--)
+	int i, j, a, k;
+	scanf("%d", &a);
+	for (i = 1; i <= a; i++)
 	{
-		for (j = 1;j <= i;j++)
+		for(k=0; k<a-i; k++)
 		{
-			printf_s("*");
+			printf(" ");
 		}
-		printf_s("\n");
+		for (j = 1; j <= i; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
 	}
+	
+	return 0;
 }
